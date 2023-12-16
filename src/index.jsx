6b8a57e7059ede50c6854ce3bc6faf1eb77c0413
chongongprecious <chongongprecious@gmail.com>
2023-12-16 11:06:18 +0100

    first commit

---
+++ src/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
+import './css/style.css';
+import Links from './Routes/Links';
+import store from './pages/Store';
+import { Provider } from 'react-redux';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+ <>
+    <Provider store={store}>
+    <Links></Links>
+    </Provider>
+ </>
+);
